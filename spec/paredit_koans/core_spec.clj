8961edc7692d36bc70591b80fee0ede35ad83c5b
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-07-14 09:17:28 -0600

    First complete draft of exercises.
    Tweaks.

--- spec/paredit_koans/core_spec.clj
+++ spec/paredit_koans/core_spec.clj
@@ -2,38 +2,72 @@
   (:require [paredit-koans.core :refer :all]
             [speclj.core :refer :all]))
 
-(def command "⌘")
-(def control "⌃")
-(def option "⌥")
-(def shift "⇧")
-(def up "↑")
-(def down "↓")
-
 ; Pre-requisites:
 ; 1. Navigation from form to form w/ ⌘ + left/right arrows
 ; 2. Expand/contract selection w/ ⌥ + up/down arrows
+; 3. Backspace & Delete (w/ ⌥)
+; 4. Formatter: ⌘⌥l
 
 (describe "Structural Editing with Paredit"
-  (it "#1 - Move Form Up/Down: ⇧⌘↑ [and] ⇧⌘↓"
+  (it "Move Form Up/Down: ⇧⌘↑ [and] ⇧⌘↓"
     (should= [:a :b :c [:d :e] :f]
              [:b [:e :d] :c :a :f]))
 
-  ;(it "#2 - Raise: ⌘'" (should= :d [:a :b :c :d :e :f :g]))
+  #_(it "Raise: ⌘'" (should= :d [:a :b :c :d :e :f :g]))
 
-  ;(it "#3 - Wrap Quotes: ⇧⌘\"" (should= ":a" :a))
-  ;(it "#4 - Wrap Square: ⇧⌘[" (should= [:a] :a))
-  ;(it "#5 - Wrap Curly: ⇧⌘{" (should= #{:a} :a))
-  ;(it "#6 - Wrap List: ⇧⌘(" (should= (print "") ""))
+  #_(it "Wrap Quotes: ⇧⌘\"" (should= ":a" :a))
+  #_(it "Wrap Square: ⇧⌘[" (should= [:a] :a))
+  #_(it "Wrap Curly: ⇧⌘{" (should= #{:a} :a))
+  #_(it "Wrap List: ⇧⌘(" (should= (print "") ""))
 
-  ;(it "#7 - Kill: ⌥⌘k"
-  ;  (should= [:a :b :c :g]
-  ;           [:a :b :c :d :e :f :g]))
+  #_(it "Kill: ⌥⌘k"
+      (should= [:a :b :c :g]
+               [:a :b :c :d :e :f :g]))
 
-  ;(it "#5 - Split - ⌃⌥s"
-  ;  (should= [[:a :b] [:c :d]]
-  ;           [[:a :b :c :d]]))
+  #_(it "Split: ⌃⌥s"
+      (should= [[:a :b] [:c :d]]
+               [[:a :b :c :d]]))
 
-  ;(it "#6 - Join - ⌃⌥j"
-  ;  (should= [[:a :b :c :d]]
-  ;           [[:a :b] [:c :d]]))
+  #_(it "Join: ⌃⌥j"
+      (should= [[:a :b :c :d]]
+               [[:a :b] [:c :d]]))
+
+  #_(it "Splice: ⌥s"
+      (should= [1 2 3 4 5]
+               [[1 2 3 4 5]]))
+
+  #_(it "Thread: ⌃⌥,"
+      (should= (->> (range) (map #(* % %)) (filter even?) (take 10) (reduce +))
+               (reduce + (take 1234 (filter even? (map #(* % %) (range))))))
+
+      #_(should= (-> "a b c d" .toUpperCase (.replace "A" "X") (.split " ") first)
+                 (first (.split (.replace (.toUpperCase "a b c d") "A" "Q") " "))))
+
+  #_(it "Un-thread: ⌃⌥."
+      (should= (reduce + (take 10 (filter even? (map #(* % %) (range)))))
+               (->> (range) (map #(* % %)) (filter even?) (take 1234) (reduce +)))
+
+      #_(should= (first (.split (.replace (.toUpperCase "a b c d") "A" "Q") " "))
+                 (-> "a b c d" .toUpperCase (.replace "A" "X") (.split " ") first)))
+
+  #_(it "Slurp Forwards: ⇧⌘k"
+      (should= [[1 2 3 4 5]]
+               [[1] 2 3 4 5]))
+
+  #_(it "Barf Forwards: ⇧⌘j"
+      (should= [[1] 2 3 4 5]
+               [[1 2 3 4 5]]))
+
+  #_(it "Slurp Backwards: ⌃⌘j"
+      (should= [[1 2 3 4 5]]
+               [1 2 3 4 [5]]))
+
+  #_(it "Barf Backwards: ⌃⌘k"
+      (should= [1 2 3 4 [5]]
+               [[1 2 3 4 5]]))
+
+  #_(it "Cycle Collection Type: ⇧⌘c [must assign in keymap]"
+      (should= [{1 2} #{3 4} [5 6]]
+               [#{1 2} [3 4] {5 6}]))
+
   )
